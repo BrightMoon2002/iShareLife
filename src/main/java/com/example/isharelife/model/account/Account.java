75092a6dfd8462c06bc7b5ca0cc44d87bb2d5392
hieuhvh1994 <hieudaohn94@gmail.com>
2022-01-10 12:16:21 +0700

    Added ChatRoom Models

--- src/main/java/com/example/isharelife/model/account/Account.java
+++ src/main/java/com/example/isharelife/model/account/Account.java
@@ -5,22 +5,22 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "accounts", uniqueConstraints = {
-        @UniqueConstraint(columnNames =  {
-                "username"
-        }),
-        @UniqueConstraint(columnNames = {
-                "email"
-        })
-})
+//@Table(name = "accounts", uniqueConstraints = {
+//        @UniqueConstraint(columnNames =  {
+//                "username"
+//        }),
+//        @UniqueConstraint(columnNames = {
+//                "email"
+//        })
+//})
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank
-    @Size(min = 3, max = 50)
-    private String name;
-    @NotBlank
-    @Size(min = 3, max = 50)
-    private String username;
+//    @NotBlank
+//    @Size(min = 3, max = 50)
+//    private String name;
+//    @NotBlank
+//    @Size(min = 3, max = 50)
+//    private String username;
 }
