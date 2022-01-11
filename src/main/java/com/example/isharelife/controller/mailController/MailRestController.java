39c0d73aebf17842528590b559ca8cdc22cc03e3
nhimdom200292 <kiemda02@gmail.com>
2022-01-11 08:39:30 +0700

    Create done mail service

---
+++ src/main/java/com/example/isharelife/controller/mailController/MailRestController.java
@@ -0,0 +1,12 @@
+package com.example.isharelife.controller.mailController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin("*")
+@RequestMapping("/mails")
+public class MailRestController {
+
+}
